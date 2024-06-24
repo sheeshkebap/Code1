f5473e9f40894a99c16927c2cf8de85d32790dd4
sheeshkebap <andr.niederreiter@gmail.com>
2024-06-24 11:49:00 +0200

    Game with functions

--- Game(functions)/test.js
+++ Game(functions)/test.js
@@ -1,4 +1,5 @@
 "use strict";
+console.log("Hello");
 let UserInput;
 function roteTür() {
     UserInput = prompt("Die Tür ist zu. Die Klinke bricht allerdings ab. Welche der beiden verbleibenden Türen willst du öffnen? 'blau' oder 'grün'");
@@ -7,12 +8,55 @@ function roteTür() {
     }
     else {
         alert("Die grüne Tür schleckt dich ab und du stirbst qualvoll!");
+        ersterRaum();
+    }
+}
+function blaueTür() {
+    UserInput = prompt("Mit der Klinke kannst du die Tür öffnen. Du schreitest durch sie hindurch. Allerdings gibt der Boden unter dir nach. Willst du dich irgendwo 'festhalten' oder einfach nur 'fallen'?");
+    if (UserInput == "festhalten") {
+        HöhleRohr();
+    }
+    else {
+    }
+}
+function HöhleRohr() {
+    UserInput = prompt("Du hälst dich an ein Rohr fest, welches jedoch abbricht, weswegen du in die Tiefe fällst. Du landest in einem kalten See in einer riesigen Hölle. Du entdeckst einen Ausgang, der von einem Felsen versperrt ist. Möchtest du diesen mit dem Rohr 'entfernen', die Höhle 'erkunden' oder auf Hilfe 'warten'?");
+    switch (UserInput) {
+        case "entfernen":
+            alert("Du gelangst ins Freie und lebst ein langes, glückliches, aber sehr armes Leben.");
+            break;
+        case "erkunden":
+            alert("Durch das Erkunden lernst du die Höhle besser kennen und kannst Sightseeing-Touren anbieten. Dadurch wirst du sehr reich und lebst zudem ein glückliches langes Leben!");
+        default:
+            alert("Du verhungerst und träumst von einem guten Leben, dass an dir vorbei zieht.");
+            ersterRaum();
+            break;
+    }
+}
+function Höhle() {
+    UserInput = prompt("Du landest in einem kalten See in einer riesigen Hölle. Möchtest du die Höhle 'erkunden' oder auf Hilfe 'warten'?");
+    if (UserInput == "erkunden") {
+        alert("Durch das Erkunden lernst du die Höhle besser kennen und kannst Sightseeing-Touren anbieten. Dadurch wirst du sehr reich und lebst zudem ein glückliches langes Leben!");
+    }
+    else {
+        alert("Du verhungerst und träumst von einem guten Leben, dass an dir vorbei zieht.");
+        ersterRaum();
     }
 }
 function ersterRaum() {
     UserInput = prompt("Du stehst in einem hellen Raum mit drei Türen. Eine ist 'grün', eine ist 'rot' und die letzte ist 'blau'. Durch welche gehst du?");
     switch (UserInput) {
         case "rot":
-            fn(roteTür);
+            roteTür();
+            break;
+        case "blau":
+            alert("Die Tür hat keine Griff! Du gehst wieder zurück.");
+            ersterRaum();
+            break;
+        default:
+            alert("Die grüne Tür schleckt dich ab und du stirbst qualvoll!");
+            ersterRaum();
+            break;
     }
 }
+ersterRaum();
--- Game(functions)/test.ts
+++ Game(functions)/test.ts
@@ -1,3 +1,5 @@
+console.log("Hello")
+
 let UserInput: string;
 
 function roteTür():void {
@@ -5,10 +7,42 @@ function roteTür():void {
         if(UserInput=="blau") {
             alert("OK")
         } else {
-            alert("Die grüne Tür schleckt dich ab und du stirbst qualvoll!")
+            alert("Die grüne Tür schleckt dich ab und du stirbst qualvoll!"); ersterRaum()
         }
 }
 
+function blaueTür():void{
+    UserInput = prompt("Mit der Klinke kannst du die Tür öffnen. Du schreitest durch sie hindurch. Allerdings gibt der Boden unter dir nach. Willst du dich irgendwo 'festhalten' oder einfach nur 'fallen'?")!;
+        if(UserInput=="festhalten"){
+            HöhleRohr()
+        } else {
+
+        }
+
+}
+
+function HöhleRohr():void{
+    UserInput = prompt("Du hälst dich an ein Rohr fest, welches jedoch abbricht, weswegen du in die Tiefe fällst. Du landest in einem kalten See in einer riesigen Hölle. Du entdeckst einen Ausgang, der von einem Felsen versperrt ist. Möchtest du diesen mit dem Rohr 'entfernen', die Höhle 'erkunden' oder auf Hilfe 'warten'?")!;
+     switch(UserInput) {
+        case "entfernen":
+            alert("Du gelangst ins Freie und lebst ein langes, glückliches, aber sehr armes Leben.");
+            break;
+        case "erkunden":
+            alert("Durch das Erkunden lernst du die Höhle besser kennen und kannst Sightseeing-Touren anbieten. Dadurch wirst du sehr reich und lebst zudem ein glückliches langes Leben!")
+        default:
+            alert("Du verhungerst und träumst von einem guten Leben, dass an dir vorbei zieht."); ersterRaum();
+            break;
+     }
+}
+
+function Höhle():void{
+    UserInput = prompt("Du landest in einem kalten See in einer riesigen Hölle. Möchtest du die Höhle 'erkunden' oder auf Hilfe 'warten'?")!;
+        if(UserInput=="erkunden"){
+            alert("Durch das Erkunden lernst du die Höhle besser kennen und kannst Sightseeing-Touren anbieten. Dadurch wirst du sehr reich und lebst zudem ein glückliches langes Leben!")
+        } else {
+            alert("Du verhungerst und träumst von einem guten Leben, dass an dir vorbei zieht."); ersterRaum();
+        }
+}
 
 function ersterRaum():void
 {
@@ -16,7 +50,15 @@ function ersterRaum():void
 
         switch(UserInput) {
             case "rot":
-                fn(roteTür);
+                roteTür();
+                break;
+            case "blau":
+                alert("Die Tür hat keine Griff! Du gehst wieder zurück."); ersterRaum();
+                break;
+            default:
+                alert("Die grüne Tür schleckt dich ab und du stirbst qualvoll!"); ersterRaum();
+                break;
         }
 }
 
+ersterRaum();
