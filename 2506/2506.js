243675dff157469ad7b28adc5db7e5bf9b1e2914
sheeshkebap <andr.niederreiter@gmail.com>
2024-06-25 11:48:39 +0200

    2506

--- 2506/2506.js
+++ 2506/2506.js
@@ -11,14 +11,17 @@ do {
     i++;
     console.log(i);
     let newSpan = document.createElement("span");
-    newSpan.textContent = "Hello";
+    newSpan.textContent = " Hello";
     document.body.appendChild(newSpan);
     newSpan.style.backgroundColor = "red";
 } while (i < 10);
 let b = 10;
+let abs = document.createElement("br");
 do {
     b++;
     console.log(b);
+    abs;
     let secondSpan = document.createElement("span");
     secondSpan.textContent = "Bye";
-} while (i > 10 && b < 30);
+    document.body.appendChild(secondSpan);
+} while (i == 10 && b < 30);
--- 2506/2506.ts
+++ 2506/2506.ts
@@ -14,16 +14,20 @@ do {
     i++;
     console.log(i);
     let newSpan: HTMLSpanElement = document.createElement("span");
-    newSpan.textContent = "Hello"
+    newSpan.textContent = " Hello"
     document.body.appendChild(newSpan);
     newSpan.style.backgroundColor = "red";
 } while (i < 10)
 
 let b: number = 10;
 
+let abs: HTMLBRElement = document.createElement("br")
+
 do {
     b++;
     console.log(b);
+    abs;
     let secondSpan: HTMLSpanElement = document.createElement("span");
-    secondSpan.textContent = "Bye"
-} while (i > 10 && b < 30)
+    secondSpan.textContent = "Bye";
+    document.body.appendChild(secondSpan);
+} while (i == 10 && b < 30)
